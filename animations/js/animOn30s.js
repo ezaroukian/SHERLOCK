19139510b62f46d1b499f46616969c7f7d9a8158
ezaroukian <egz@jhu.edu>
2015-11-12 08:35:29 -0500

    Add animation files

---
+++ animations/js/animOn30s.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#animOn30s').scianimator({
+	    'images': ['animOn30s/animOn30s1.png', 'animOn30s/animOn30s2.png', 'animOn30s/animOn30s3.png', 'animOn30s/animOn30s4.png', 'animOn30s/animOn30s5.png', 'animOn30s/animOn30s6.png', 'animOn30s/animOn30s7.png', 'animOn30s/animOn30s8.png', 'animOn30s/animOn30s9.png', 'animOn30s/animOn30s10.png', 'animOn30s/animOn30s11.png', 'animOn30s/animOn30s12.png', 'animOn30s/animOn30s13.png', 'animOn30s/animOn30s14.png', 'animOn30s/animOn30s15.png', 'animOn30s/animOn30s16.png', 'animOn30s/animOn30s17.png', 'animOn30s/animOn30s18.png', 'animOn30s/animOn30s19.png', 'animOn30s/animOn30s20.png', 'animOn30s/animOn30s21.png', 'animOn30s/animOn30s22.png', 'animOn30s/animOn30s23.png', 'animOn30s/animOn30s24.png', 'animOn30s/animOn30s25.png', 'animOn30s/animOn30s26.png', 'animOn30s/animOn30s27.png', 'animOn30s/animOn30s28.png', 'animOn30s/animOn30s29.png', 'animOn30s/animOn30s30.png', 'animOn30s/animOn30s31.png', 'animOn30s/animOn30s32.png', 'animOn30s/animOn30s33.png', 'animOn30s/animOn30s34.png', 'animOn30s/animOn30s35.png', 'animOn30s/animOn30s36.png', 'animOn30s/animOn30s37.png', 'animOn30s/animOn30s38.png', 'animOn30s/animOn30s39.png', 'animOn30s/animOn30s40.png', 'animOn30s/animOn30s41.png', 'animOn30s/animOn30s42.png', 'animOn30s/animOn30s43.png', 'animOn30s/animOn30s44.png', 'animOn30s/animOn30s45.png', 'animOn30s/animOn30s46.png', 'animOn30s/animOn30s47.png', 'animOn30s/animOn30s48.png', 'animOn30s/animOn30s49.png', 'animOn30s/animOn30s50.png', 'animOn30s/animOn30s51.png', 'animOn30s/animOn30s52.png', 'animOn30s/animOn30s53.png', 'animOn30s/animOn30s54.png', 'animOn30s/animOn30s55.png', 'animOn30s/animOn30s56.png', 'animOn30s/animOn30s57.png', 'animOn30s/animOn30s58.png', 'animOn30s/animOn30s59.png', 'animOn30s/animOn30s60.png', 'animOn30s/animOn30s61.png', 'animOn30s/animOn30s62.png', 'animOn30s/animOn30s63.png', 'animOn30s/animOn30s64.png', 'animOn30s/animOn30s65.png', 'animOn30s/animOn30s66.png', 'animOn30s/animOn30s67.png', 'animOn30s/animOn30s68.png', 'animOn30s/animOn30s69.png', 'animOn30s/animOn30s70.png', 'animOn30s/animOn30s71.png', 'animOn30s/animOn30s72.png'],
+	    'width': 480,
+	    'delay': 150,
+	    'loopMode': 'loop'
+	});
+	$('#animOn30s').scianimator('play');
+    });
+})(jQuery);
